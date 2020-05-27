ea4718be8e9e82d43c2e506a121d25f0c29f4dae
Kathleen Cavanagh <kathleen.a.cavanagh@gmail.com>
2020-05-27 13:41:10 -0400

    remove constraints which are no longer used

--- dev/test_traj_opt.py
+++ dev/test_traj_opt.py
@@ -8,10 +8,6 @@ cp = CartPole()
 s0 = np.array([0, 0, 0, 0])
 sf = np.array([0, np.pi, 0, 0])
 
-constraints = {
-    'u': 20, 'state': np.full(cp.n_state, np.nan)
-}
-
 traj_opt = SystemTrajectoryOptimization(cp, 50, knot_points = 500, slack = .01)
 
 traj_opt.add_direct_collocation_constraints(s0, sf)
